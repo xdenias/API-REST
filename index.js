b090aba97891c1295b7f78bada944796941f3e94
Denilson Santos <denilson.avsantos@hotmail.com>
2020-07-19 21:58:38 -0300

    criando utiliade de verificar erro

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-consign().include("routes").into(app);
+consign().include("routes").include("utils").into(app);
 
 app.listen(3000, "127.0.0.1", () => {
   console.log("Servidor rodando");
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,14 @@ let db = new NeDB({
 });
 
 module.exports = (app) => {
-  app.get("/users", (req, res) => {
+  let route = app.route("/users");
+
+  route.get((req, res) => {
     db.find({})
       .sort({ name: 1 })
       .exec((err, users) => {
         if (err) {
-          console.log(`error:${err}`);
-          res.status(400);
+          app.utils.error.send(err, req, res);
         } else {
           res.statusCode = 200;
           res.setHeader("Content-Type", "application/json");
@@ -22,11 +23,10 @@ module.exports = (app) => {
       });
   });
 
-  app.post("/users", (req, res) => {
+  route.post((req, res) => {
     db.insert(req.body, (err, user) => {
       if (err) {
-        console.log(`error:${err}`);
-        res.status(400).json({ error: err });
+        app.utils.error.send(err, req, res);
       } else {
         res.status(200).json(user);
       }
