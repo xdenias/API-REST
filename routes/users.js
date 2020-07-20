d3df9633d7ccfa255f261d3270c8393ef3ce4d1f
Denilson Santos <denilson.avsantos@hotmail.com>
2020-07-19 22:07:54 -0300

    Listando um usuario por vez

--- routes/users.js
+++ routes/users.js
@@ -32,4 +32,16 @@ module.exports = (app) => {
       }
     });
   });
+
+  let routeId = app.route("/users/:id");
+
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
 };
