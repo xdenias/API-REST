fc3021a746fd49d88996926340f9a50fd89912ee
Denilson Santos <denilson.avsantos@hotmail.com>
2020-07-19 23:04:35 -0300

    Excluindo usuário

--- routes/users.js
+++ routes/users.js
@@ -50,7 +50,17 @@ module.exports = (app) => {
       if (err) {
         app.utils.error.send(err, req, res);
       } else {
-        res.status(200).json(req.body);
+        res.status(200).json(Object.assign(req.params, req.body));
+      }
+    });
+  });
+
+  routeId.delete((req, res) => {
+    db.remove({ _id: req.params.id }, {}, (err) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(Object.assign(req.params, req.body));
       }
     });
   });
